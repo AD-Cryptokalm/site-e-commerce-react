c56a1f2fc46f406ab7f6b7c9ef23057dba6ac907
AD-Cryptokalm <cryptokalm82@gmail.com>
2022-12-03 17:56:47 +0100

    categories

---
+++ client/src/components/footer.js
@@ -0,0 +1,18 @@
+import '../styles/footer.scss'
+
+export default function footer() {
+  return (
+    <div className='footer'>
+        <h3>A votre service</h3>
+        <ul className="footer-links">
+            <li className="footer-link">Condition</li>
+            <li className="footer-link"></li>
+            <li className="footer-link"></li>
+            <li className="footer-link"></li>
+            <li className="footer-link"></li>
+            <li className="footer-link"></li>
+            <li className="footer-link"></li>
+        </ul>
+    </div>
+  )
+}
