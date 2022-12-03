af1f49b5fdbcdc61c664f87edca47b7f5918a9fe
AD-Cryptokalm <cryptokalm82@gmail.com>
2022-12-03 23:14:09 +0100

    page d'accueil

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SlideAccueil from './SlideAccueil'
+import Categorie from './Categorie'
+
+export default function Home() {
+  return (
+    <>
+    <SlideAccueil/>
+    <Categorie/>
+    </>
+  )
+}
