0bd32e6cc6eb6df19b3f9aa3a6e8e388e08a3cbd
AD-Cryptokalm <cryptokalm82@gmail.com>
2022-12-06 17:07:31 +0100

    Auth

--- client/src/components/Log/LoginForm.js
+++ client/src/components/Log/LoginForm.js
@@ -1,18 +1,16 @@
 import "../../styles/loginForm.scss";
 import React, { useState } from "react";
 import axios from "axios";
+import Home from "../Home/Home";
 // import Home from "../Home/Home";
 
 export default function LoginForm() {
-  // const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleChange = (e) => {
     e.preventDefault();
-    // setEmail(e.target.value);
-    // setPassword(e.target.value);
-    // console.log(e.target.value);
     axios({
       method: "POST",
       url: `http://localhost:3000/login`,
@@ -24,6 +22,7 @@ export default function LoginForm() {
     })
       .then((res) => {
         console.log(res.data);
+        setIsLogin(true);
       })
       .catch((err) => {
         console.log(err);
@@ -33,29 +32,35 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="loginForm">
-      <h2>Connectez-vous !</h2>
-      <form action="" onSubmit={handleChange} id="form">
-        <input
-          type="text"
-          name="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          placeholder="Password"
-        />
-        <h3>Mot de passe oublié ?</h3>
+    <>
+      {isLogin ? (
+        <Home />
+      ) : (
+        <div className="loginForm">
+          <h2>Connectez-vous !</h2>
+          <form action="" onSubmit={handleChange} id="form">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Password"
+            />
+            <h3>Mot de passe oublié ?</h3>
 
-        <input className="btn-form" type="submit" value="Se Connecter"/>
-      </form>
-    </div>
+            <input className="btn-form" type="submit" value="Se Connecter" />
+          </form>
+        </div>
+      )}
+    </>
   );
 }
--- client/src/components/Log/RegisterForm.js
+++ client/src/components/Log/RegisterForm.js
@@ -1,14 +1,84 @@
-import React from 'react'
+import "../../styles/loginForm.scss";
+import React, { useState } from "react";
+import axios from "axios";
+import Home from "../Home/Home";
+// import Home from "../Home/Home";
 
 export default function RegisterForm() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: `http://localhost:3000/register`,
+      withCredentials: true,
+      data: {
+        firstname,
+        lastname,
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setIsLogin(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setEmail("");
+        setPassword("");
+      });
+  };
+
   return (
-    <div className='registerForm'>
-      <form action="POST" className="registerForm-form">
-        <label htmlFor="Nom"></label>
-        <input type="text" name="firstname" id="firstname" />
-        <label htmlFor="Prénom"></label>
-        <input type="text" name="lastname" id="lastname" />
-      </form>
-    </div>
-  )
+    <>
+      {isLogin ? (
+        <Home />
+      ) : (
+        <div className="loginForm">
+          <h2>Inscrivez-vous !</h2>
+          <form action="" onSubmit={handleChange} id="form">
+            <input
+              type="text"
+              name="firstname"
+              id="firstname"
+              onChange={(e) => setFirstname(e.target.value)}
+              value={firstname}
+              placeholder="Nom"
+            />
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              onChange={(e) => setLastname(e.target.value)}
+              value={lastname}
+              placeholder="Prénom"
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Password"
+            />
+            <input className="btn-form" type="submit" value="Se Connecter" />
+          </form>
+        </div>
+      )}
+    </>
+  );
 }
