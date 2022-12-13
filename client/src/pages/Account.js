3ae51ef84b9dd3cf56f48a384d7a4979c77b5b0f
AD-Cryptokalm <cryptokalm82@gmail.com>
2022-12-13 17:35:49 +0100

    account

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import '../styles/account.scss'
 
 export default function Account() {
+  const userData = useSelector((state) => state.userReducer);
+  console.log(userData);
+
   return (
-    <div>Account</div>
-  )
+    <div className="account">
+      <h2 className="account-tilte">Mon Compte</h2>
+      <div className="account-block">
+      <div className="account-left">
+        <h3 className="account-title">Historique des commandes</h3>
+        <p>Vous n'avez pas encore passé de commandes</p>
+      </div>
+      <div className="account-right">
+        <h3 className="account-title">Détails du compte</h3>
+        <p className="account-name">
+          {userData.firstname} {userData.lastname}
+        </p>
+        <div className="account-adress">{userData.adress}</div>
+      </div>
+      </div>
+    </div>
+  );
 }
