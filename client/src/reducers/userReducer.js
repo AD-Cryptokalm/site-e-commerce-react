d5c58eeed47dddec4ed5f6383abb2276fbfe656b
AD-Cryptokalm <cryptokalm82@gmail.com>
2022-12-15 02:15:08 +0100

    redux update profil

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { GET_USER } from "../actions/userAction";
+import { GET_USER, UPDATE_EMAIL, UPDATE_LASTNAME, UPDATE_FIRSTNAME, UPDATE_ADRESS, UPDATE_POSTALCODE, UPDATE_CITY  } from "../actions/userAction";
 
 const initialState = {};
 
@@ -6,6 +6,36 @@ export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case GET_USER:
       return action.payload;
+      case UPDATE_EMAIL:
+      return {
+        ...state,
+        email: action.payload,
+      };
+      case UPDATE_FIRSTNAME:
+      return {
+        ...state,
+        firstName: action.payload,
+      }
+      case UPDATE_LASTNAME:
+      return {
+        ...state,
+        lastname: action.payload,
+      }
+      case UPDATE_ADRESS:
+        return {
+          ...state,
+          adress: action.payload,
+        }
+        case UPDATE_POSTALCODE:
+          return {
+            ...state,
+            postalCode: action.payload
+          }
+          case UPDATE_CITY:
+      return {
+        ...state,
+        city: action.payload,
+      }
       default:
         return state
   }
